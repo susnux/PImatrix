76a28713eae69eee7e586025437e59ae09e70ebc
Ferdinand Thiessen <rpm@fthiessen.de>
2021-03-07 17:51:49 +0100

    Pixel fonts for text mode

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ import setuptools
 
 setuptools.setup(
     name="PImatrix",
-    version="0.0.1",
+    version="0.0.2",
     author="Ferdinand Thiessen",
     author_email="rpm@fthiessen.de",
     description="Controller for the stage LED matrix @ClubAquariumDD",
+    include_package_data=True,
     packages=setuptools.find_packages(),
+    package_data={'': ['data/*.pkl']},
     python_requires=">=3.6",
     install_requires=["pyaudio", "numpy", "rpi_ws281x"],
-    scripts=['bin/pimatrix']
+    scripts=['bin/pimatrix'],
 )
